006450b007a99bec5134204bfae3454ae4981d4c
techscaleups <info.techscaleup@gmail.com>
2025-06-28 08:37:41 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+ // Change navbar on scroll
+  window.addEventListener("scroll", function () {
+    document.querySelector(".navbar").classList.toggle("scrolled", window.scrollY > 10);
+  });
+
+
+   fetch("footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer-placeholder").innerHTML = data;
+    });
+
+     fetch("navbar.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("navbar-placeholder").innerHTML = data;
+    });
+
+      fetch("coming-banner.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("coming-banner-placeholder").innerHTML = data;
+    });
